9fdc24ade2805734681c45e5464ceaa77bcd20e5
Yana Vovk <yanavovk1990@outlook.com>
2021-03-24 04:28:27 +0200

    Добавляет настройки вебпака

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import variable from './module-1';
+import './scss/styles.scss';
+
+console.log(variable);
+console.log('Hello Yana!!!!');
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/i,
+            use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+        }],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' }), new MiniCssExtractPlugin({ filename: 'mystyles.css' }), new CleanWebpackPlugin()],
+     devServer: {
+         clientLogLevel: 'error',
+         stats: 'errors-only',
+  },
+};
